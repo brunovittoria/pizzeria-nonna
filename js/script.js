0ad27a0609b8cb5604f27e1a83bc2ff8b603a19f
Bruno Vittoria <brunovittoria1999@hotmail.com>
2023-01-08 20:51:53 +0100

    Adding modal window

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 pizzaJson.map((item, index ) => {
-    //console.log(item)
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
-    //console.log(pizzaItem)
+    
     document.querySelector('.pizza-area').append(pizzaItem)
 
     // preencher os dados de cada pizza
@@ -9,5 +8,13 @@ pizzaJson.map((item, index ) => {
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+
+    // Pizza clicada
+    pizzaItem.querySelector('.pizza-item a').addEventListener('click', (e) => {
+        e.preventDefault()
+        console.log('Clicou na pizza')
+
+        document.querySelector('.pizzaWindowArea').style.display = 'flex'
+    })
 
 })
